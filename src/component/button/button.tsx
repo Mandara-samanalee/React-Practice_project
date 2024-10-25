a076ce1a765c16b92fce0616383cfc7121158bd2
Mandara-samanalee <samanaleerktm.21@uom.lk>
2024-10-25 17:26:10 +0530

    create table component
    Add changes to table component

--- src/component/button/button.tsx
+++ src/component/button/button.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+//import React from 'react';
 
-const button = () => {
+const Button: React.FC = () => {
     return (
         <div className='w-20 bg-purple-400 rounded shadow-2xl'>
             <h1>This is a button</h1>
         </div>
-    )
+    );
 }
 
-export default button
+export default Button;
