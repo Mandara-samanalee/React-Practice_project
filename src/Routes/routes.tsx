22fe62fc510b55cc1520c2e4fbca987b8cf9316b
Mandara-samanalee <samanaleerktm.21@uom.lk>
2024-10-25 17:32:58 +0530

    Add about us page

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Test from '../page/test/test'
+import AboutUs from '../page/AboutUs/AboutUs'
 
 const routes = () => {
   return (
     <div>
         <Routes>
-            <Route path="/" element={<Test/>} />
+        <Route path="/" element={<Test />} />
+        <Route path="/About" element={<AboutUs />} />
         </Routes>
-          
     </div>
   )
 }
